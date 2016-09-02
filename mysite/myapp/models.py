d388632e67fd42c2e462bc04d62c6af5c9679fb2
BENEDEK László <laci@benedekl.info>
2016-09-02 15:29:05 +0200

    book model létrehozása

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    rating = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return "{t} (r)".format(t=self.title, r=self.rating)
