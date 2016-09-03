1dcd83ea2b1d3f9a1d4f40b46da0f3588d394393
BENEDEK László <laci@benedekl.info>
2016-09-03 17:46:49 +0200

    függőségek javítása

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,15 @@ set -e
 set -u
 
 # függőségek a rendszeren:
-# apt-get install python3 python3-pip
+# apt-get install python3 python3-pip python3-venv
 
 VENV_NAME=venv
 
 cd $(dirname "$0")
 
 if [ -d ./"$VENV_NAME" ] ; then
-  echo "venv already exists in dir \"$VENV_NAME\""
+  echo "venv already exists in dir \"$VENV_NAME\", to activate it type:"
+  echo ". \"$VENV_NAME/bin/activate\""
   exit 1
 fi
 
