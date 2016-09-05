06a59d065fd9547ccb68c26f43e2abb3120cb2bd
BENEDEK László <laci@benedekl.info>
2016-09-05 10:04:34 +0200

    kliens: ajax prefilter, hogy a POST és PUT JSON-t küldjön

--- mysite/myapp/static/webapp/app.js
+++ mysite/myapp/static/webapp/app.js
@@ -2,6 +2,7 @@ import Map from "can/map/";
 import route from "can/route/";
 import 'can/map/define/';
 import 'can/route/pushstate/';
+import "./util/prefilter";
 
 const AppViewModel = Map.extend({
   define: {
